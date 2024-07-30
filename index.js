4b3f2afe4b07846a2a8bd8ff616d448cebc3a284
shiva <shiva_05@outlook.com>
2024-07-30 22:20:54 +0530

    temp1

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const ws_1 = require("ws");
-const app = (0, express_1.default)();
-const httpServer = app.listen(8080);
-const wss = new ws_1.WebSocketServer({ server: httpServer });
-wss.on('connection', function connection(ws) {
-    ws.on('error', console.error);
-    ws.on('message', function (data, isBinary) {
-        wss.clients.forEach(function (client) {
-            if (client.readyState === ws_1.WebSocket.OPEN) {
-                client.send(data, { binary: isBinary });
-            }
-        });
-    });
-    ws.send('Hello! Message From Server!!');
-});
+import express from 'express'
+import { WebSocket, WebSocketServer } from 'ws'
+
+const app = express()
+
+app.get("/test",(req,res)=>{
+  return res.json({msg:"get"})
+})
+
+
+app.post("/test",(req,res)=>{
+  return res.json({msg:"post"})
+})
